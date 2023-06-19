cebbc9955f1382d552d2bf7b466ff763cc7c129c
Ricardo Sa <ricardo.sa@shopee.com>
2023-06-19 20:34:48 -0300

    feat: carousel

--- src/components/Contato/index.jsx
+++ src/components/Contato/index.jsx
@@ -12,11 +12,11 @@ function Contato(){
                             <p> <strong>Nosso objetivo é oferecer as melhores soluções para nossos clientes! </strong></p>
                         </div>
                         <div class="col-sm">
-                            <p><strong>Atendimento</strong></p>
+                            <p><strong>Solicite a visita de nosso representante:</strong></p>
                             <i class="fa fa-regular fa-phone"></i> (19) 3865-7350 <br />
                             <i class="fab fa-brands fa-whatsapp"></i> (19) 98210-9681 <br />
                             <i class="far fa-regular fa-envelope"></i>
-                            <a href='mailto:123@ecm.com.br'> abc@ecm.com.br</a>
+                            <a href='mailto:123@ecm.com.br'><span class="text-link">abc@ecm.com.br</span> </a>
                         </div>
                         <div class="col-sm">
                             <p><strong>Nossas redes</strong></p>
@@ -25,7 +25,7 @@ function Contato(){
                             <i class="fab fa-brands fa-linkedin"></i> Linkedin <br />
 
                             <i class="fab fa-brands fa-instagram"></i>
-                            <a href='https://www.instagram.com/ecmindustrial/'>Instagram</a>
+                            <a href='https://www.instagram.com/ecmindustrial/'> <span class="text-link">Instagram</span> </a>
                         </div>
                     </div>
                 </div>
--- src/components/Servicos/index.jsx
+++ src/components/Servicos/index.jsx
@@ -1,13 +1,5 @@
-import eletrica from "../../assets/eletrica.jpg"
-import caldera from "../../assets/caldera.jpg"
-import arcond from "../../assets/arcond.jpg"
-import civil from "../../assets/civil.jpg"
-import cozinha from "../../assets/cozinha.jpg"
-import laudo from "../../assets/laudo.jpg"
-import terceiro from "../../assets/terceiro.jpg"
-import { Helmet } from "react-helmet";
-
 import "../../assets/style/main.css"
+import ControlledCarousel from "./ControlledCarousel"
 
 
 function Servicos(){
@@ -16,58 +8,9 @@ function Servicos(){
             <h3>Serviços</h3>
             <p>Soluções customizadas para setores estratégicos.</p>
         </div>
+        <ControlledCarousel />
         <div class="carrosel-servico">
-            <div class="owl-carousel owl-theme">
-                <div class="item">
-                    <img class="box" src={eletrica} alt="eletrica" srcset="" />
-                    <div class="legenda">
-                        <p>Elétrica</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="box" src={caldera} alt="caldera" srcset="" />
-                    <div class="legenda">
-                        <p>Calderaria</p>
-                    </div>
-                </div>
-                <div class="item"> 
-                    <img class="box" src={arcond} alt="arcond" srcset="" /> 
-                    <div class="legenda">
-                        <p>Refrigeração</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="box" src={civil} alt="civil" srcset="" />
-                    <div class="legenda">
-                        <p>Construção</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="box" src={cozinha} alt="cozinha" srcset="" />
-                    <div class="legenda">
-                        <p>Cozinha</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="box" src={laudo} alt="laudo" srcset="" />
-                    <div class="legenda">
-                        <p>Laudo</p>
-                    </div>
-                </div>
-                <div class="item">
-                    <img class="box" src={terceiro} alt="terceiro" srcset="" />
-                    <div class="legenda">
-                        <p>Terceirização</p>
-                    </div>
-                </div>
-            </div>
         </div>
-
-        <Helmet>
-            <script src="./style/js/owl/jquery.min.js"></script>
-            <script src="./style/js/owl/owl.carousel.min.js"></script>
-            <script src="./style/js/owl/setup.js"></script>
-        </Helmet>
     </section>
 }
 
