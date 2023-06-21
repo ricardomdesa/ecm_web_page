53280c27ac6bdd53dba4739e2fa770ff74c5e263
Patricia Fonseca <fonseca.paty@gmail.com>
2023-06-20 21:25:12 -0300

    add Modal

--- src/components/Servicos/index.jsx
+++ src/components/Servicos/index.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "../../assets/style/main.css"
+import ModalServicos from "../ModalServicos"
+import Button from 'react-bootstrap/Button';
 import ControlledCarousel from "./ControlledCarousel"
 
 
 function Servicos(){
+    const [modalShow, setModalShow] = useState(false);
     return <section id="servicos">
         <div class="container-fluid servicos text-center">
             <h3>Serviços</h3>
             <p>Soluções customizadas para setores estratégicos.</p>
         </div>
         <ControlledCarousel />
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+        modal 1
+        </Button>
+        <ModalServicos
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        />
         <div class="carrosel-servico">
         </div>
     </section>
